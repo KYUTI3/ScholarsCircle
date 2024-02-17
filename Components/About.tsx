c3074dfe6c97710641dc9d226d3b0b2d3e2a2ac5
RainIsBetter <160345844+RainIsBetter@users.noreply.github.com>
2024-02-17 15:47:00 -0800

    about us text

---
+++ Components/About.tsx
@@ -0,0 +1,6 @@
+"Communicating in life is stressful, but when a screen is in between you and a person all that anxiety goes away.
+Making friends your age is tough too, as you have to initiate the conversation and overcome the fear of judgment.
+Online there are some alternatives where you can meet strangers online, but it is unsafe and you can see some pretty 
+disturbing things. Thus, we created the Scholars' Circle, the first website you can use in order to meet strangers 
+your age that live nearby. Scholars' Circle was made to be used to make online friends that you know are not creepy old men,
+which could go on to become your permanent real life friends. Be safe, use Scholars' Circle."
