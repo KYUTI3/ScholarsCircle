86a9f521ca0c8af915d4e497109f9cec89618e7e
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-02-17 11:28:40 -0800

    Footer component + misc

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
+import github from '/assets/github.svg'
 
 const Footer = () => {
+<div className="items-center " > 
+    <div> 
+     ScholarsCircle © 2024
+    </div>
+    <div> 
+     Made by Luis Cardenas, Anna Katkova, Kaelyn Tan & ???
+    </div>
+    <Image
+     src={github.svg}
+     alt="Our GitHub Repository" 
+    />
+</div>
 
 
 
 
 
-    
 }
