1f224cab425de13325bd12509e3a07ed04481218
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-02-17 18:56:31 -0800

    About component

--- Components/About.tsx
+++ Components/About.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const About = () => {
 return(
-  <div className="text-center text-[8px] text-cyan-500 bg-gray-800 border-2 rounded">
+  <div className="relative max-w-md mx-auto p-4 text-center text-[8px] text-cyan-500 bg-gray-800 border-2 rounded-md space-y-2 font-sans -translate-y-50">
     <div className="font-bold text-[10px]">
         Why ScholarCircle?
     </div>
-    <div> 
+    <div className="subpixel-antialiased"> 
      Communicating in life is stressful, but when a screen is in between you and a person all that anxiety goes away.
      Making friends your age is tough too, as you have to initiate the conversation and overcome the fear of judgment.
      Scholars' Circle was made to be used to make online friends that you know are not creepy. Which could go on to become your permanent real life friends.
-   </div>
+    </div>
 </div>
  )
 }
