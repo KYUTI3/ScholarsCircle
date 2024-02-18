1c1c1863ac6c7cea38cbe8127570d47d479a0808
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-02-18 01:46:43 -0800

    minor fixes

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 
 const Footer = () => {
 return(
-    <div className="items-center space-x-2 mt-2 top-0 place-content-center" > 
-        <div> 
+    <div className="flex flex-row items-center space-x-4 mt-2 mb-2 top-0 place-content-center text-gray-800 font-bold font-sans text-[20px] py-3" > 
+        <div className="hover:text-cyan-500"> 
          Home
         </div>
-        <div> 
+        <div className="hover:text-cyan-500"> 
          About 
         </div>
-        <div> 
+        <div className="hover:text-cyan-500"> 
          Profile
         </div>
     </div>
