92c642eeb617163677989bfaa36f7bb73073898d
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-02-17 17:28:27 -0800

    About component v1

--- Components/About.tsx
+++ Components/About.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const About = () => {
 return(
-<div> 
-    Communicating in life is stressful, but when a screen is in between you and a person all that anxiety goes away.
-    Making friends your age is tough too, as you have to initiate the conversation and overcome the fear of judgment.
-    Thus, we created the Scholars' circle. Scholars' Circle was made to be used to make online friends that you know are not creepy old men,
-    which could go on to become your permanent real life friends.
-</div>
+  <div className="text-center text-[12px] bg-blue-600 slate-700 border-2 rounded ">
+
+     Communicating in life is stressful, but when a screen is in between you and a person all that anxiety goes away.
+     Making friends your age is tough too, as you have to initiate the conversation and overcome the fear of judgment.
+     Thus, we created the Scholars' circle. Scholars' Circle was made to be used to make online friends that you know are not creepy,
+     which could go on to become your permanent real life friends.
+
+  </div>
  )
 }
+
+export default About;
