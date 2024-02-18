08e86f0122bd0e4b6d44360e2158581edb6d1980
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-02-17 16:36:12 -0800

    small changes to components (Footer, Form)

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import github from '/public/github.svg'
 
 
 const Footer = () => {
 return(
- <div className="fixed flex bottom-0 left-0 w-full bg-gray-300 py-4 px-6 justify-center items-center "> 
-   <div className="flex flex-col text-center space-y-1.5"> 
+ <div className="fixed flex bottom-0 left-0 w-full bg-gray-300 py-2 px-4 justify-center items-center "> 
+   <div className="flex flex-col text-center space-y-2"> 
     <div> 
      ScholarsCircle © 2024
     </div>
---
+++ Components/Form.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { Input } from 'postcss'
+
+
+
+const Form = (Input) => {
+return(
+    <div className="absolute self-center ">
+     <form>
+        <div>
+            <Input name="" />
+
+
+
+        </div>
+
+
+     </form>
+    </div>
+ )
+}
